bf18f6f20deaec0191ff27fddb88183d6fc12459
wozmir <wozmir@gmail.com>
2016-08-16 20:54:18 -0500

    initialize repository

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,9 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+   def hello
+    render html: "¡hola, mundo!"
+  end
+  def goodbye
+    render html: "¡adios, mundo!"
+  end
+end
